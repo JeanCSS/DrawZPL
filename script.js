63cce20d1b1842cff6d45033298ad9bac2e25527
JeanCSS <jeancordeiro626@gmail.com>
2024-02-24 22:04:26 -0300

    1.0.0.0.1

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+
+Draw = () => {
+    // Desenha um retângulo vermelho
+    ctx.fillStyle = 'red';
+    ctx.fillRect(50, 50, 100, 100);
+
+    // Desenha um círculo azul
+    ctx.beginPath();
+    ctx.arc(200, 200, 50, 0, Math.PI * 2);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.closePath();
+}
+
 // Obtém o elemento canvas e seu contexto
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-// Desenha um retângulo vermelho
-ctx.fillStyle = 'red';
-ctx.fillRect(50, 50, 100, 100);
-
-// Desenha um círculo azul
-ctx.beginPath();
-ctx.arc(200, 200, 50, 0, Math.PI * 2);
-ctx.fillStyle = 'blue';
-ctx.fill();
-ctx.closePath();
+Draw();
